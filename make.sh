2291616d1e6e91dbe9e5f837dda07dd01176ca2f
Jan Kuča <jan@jankuca.com>
2012-03-05 16:42:21 +0100

    improve make.sh, add the boilerplate without fast-forwarding + add a welcome message + ask for project name The original branch is renamed to "temp", a new empty "origin" branch is created and a "package.json" file is commited to the new branch. The original (temp) branch is then merged into the new branch without fast-forwarding (--no-ff) to keep the closure-boilerplate history separate from the actual project.

--- make.sh
+++ make.sh
@@ -3,6 +3,12 @@
 # --
 # @autor Jan Kuča
 
+# Welcome message
+echo ""
+echo "Closure-Boilerplate Installation Script"
+echo "======================================="
+echo ""
+
 
 
 # Boilerplate git remote name
@@ -23,12 +29,49 @@ CLOSURE_COMPILER_PATH=$PROJECT_DIR/build/closure-compiler
 # The Google Closure Library URL
 CLOSURE_LIBRARY_ARCHIVE_NAME="closure-library-20111110-r1376.zip"
 CLOSURE_LIBRARY_URL="http://closure-library.googlecode.com/files/$CLOSURE_LIBRARY_ARCHIVE_NAME"
-echo $CLOSURE_LIBRARY_URL
 
 # The Google Closure Compiler URL
 CLOSURE_COMPILER_ARCHIVE_NAME="compiler-latest.zip"
 CLOSURE_COMPILER_URL="http://closure-compiler.googlecode.com/files/$CLOSURE_COMPILER_ARCHIVE_NAME"
 
+
+
+# Check the repository
+if ! git checkout master ; then
+  echo ""
+  echo "! Invalid repository"
+  echo "This has to be a cloned closure-boilerplate repository."
+  echo "Follow the installation instructions on GitHub."
+  exit 1
+fi
+
+
+# Ask for the project name
+read -p "Name of this project in package.json: [app] " APP_NAME
+[ -z $APP_NAME ] && APP_NAME="app"
+
+# Rename the original branch to temp
+git branch -m master temp
+# Create a new empty branch called master
+git checkout --orphan master
+# Remove the original (temp) branch
+git branch -D temp
+# Clear the working directory
+git clean -dfx
+
+
+# Create a package.json file
+# Note: We cannot commit without any files added to the repository
+#   and a package.json file is not completely useless.
+echo -e "{\n  \"name\": \"$APP_NAME\"\n}" > $PROJECT_DIR/package.json
+# Stage the created package.json file
+git add $PROJECT_DIR/package.json
+# Commit the file as the project's initial commit
+git commit -m 'initial commit'
+
+
+# Merge the original branch to the new master branch without fast-forwarding
+git merge origin/master --no-ff -m 'add closure-boilerplate'
 
 
 # Rename the remote to boilerplate
@@ -37,11 +80,13 @@ git remote rename origin $GIT_REMOTE_NAME
 
 # Download the Google Closure Library
 curl $CLOSURE_LIBRARY_URL > $CLOSURE_LIBRARY_ARCHIVE_NAME
+mkdir -p $CLOSURE_LIBRARY_PATH
 unzip $CLOSURE_LIBRARY_ARCHIVE_NAME -d $CLOSURE_LIBRARY_PATH
 rm $CLOSURE_LIBRARY_ARCHIVE_NAME
 
 
 # Download the Google Closure Library
 curl $CLOSURE_COMPILER_URL > $CLOSURE_COMPILER_ARCHIVE_NAME
+mkdir -p $CLOSURE_COMPILER_PATH
 unzip $CLOSURE_COMPILER_ARCHIVE_NAME -d $CLOSURE_COMPILER_PATH
 rm $CLOSURE_COMPILER_ARCHIVE_NAME
