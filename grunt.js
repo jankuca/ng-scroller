c72bfb9d4dde7c237e8484336e829a0371fd75c2
jankuca <jan@jankuca.com>
2013-03-10 01:14:37 +0100

    add `grunt soy`

--- grunt.js
+++ grunt.js
@@ -17,6 +17,9 @@ module.exports = function (grunt) {
       compile: {
         command: './build/compile.sh 2>&1 > /dev/null'
       },
+      soy: {
+        command: './build/soy.sh'
+      },
       rework: {
         command: './build/rework.js'
       }
@@ -27,12 +30,17 @@ module.exports = function (grunt) {
 
   grunt.registerTask('default', [
     'css',
+    'soy',
     'js'
   ]);
 
   grunt.registerTask('css', [
     'concat:css',
     'exec:rework',
+  ]);
+
+  grunt.registerTask('soy', [
+    'exec:soy',
   ]);
 
   grunt.registerTask('js', [
